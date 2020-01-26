37de49f0e76fd12775ca5ec3fd343cc735507cdd
calm-fork-7535 <komald.4@gmail.com>
2020-01-26 19:58:46 +0000

    Done.

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,12 @@
-require_relative "blackjack.rb"
-
-runner
+# require_relative "blackjack.rb"
+# def runner
+#   welcome
+#   total = initial_round
+#
+#   until total >= 21 do
+#     total = hit?(total)
+#   end
+#   end_game(total)
+#
+# end	end
+# runner

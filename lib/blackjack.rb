cabff0fabac34a38788f74a956fafd2dfb62b7a3
Jared Fleming <jaredmf10@gmail.com>
2020-01-28 16:15:42 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,15 +25,25 @@ def end_game(display_card_total)
 end
 
 def initial_round
-  deal_card
-  deal_card
-  display_card_total(deal_card)
-end
+  total = deal_card + deal_card
+  display_card_total(total)
+  total
 end
 
-def hit?
+def hit?(card_total)
+  valid_inputs = ["h", "s"]
+
   prompt_user
-  get_user_input
+  user_input = get_user_input
+
+  if user_input == "h"
+    card_total += deal_card
+  elsif user_input == "s"
+    card_total
+  else
+    invalid_command
+    hit?(card_total)
+  end
 end
 
 def invalid_command
@@ -45,5 +55,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+      card_total = hit?(card_total)
+      display_card_total(card_total)
+    end
+    end_game(card_total)
 end
